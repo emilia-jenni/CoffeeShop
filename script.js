7cdd0767d53d8de974cc795a86c831313289971e
Vuorenmaa Emilia <s2100154@edu.bc.fi>
2021-09-17 15:20:27 +0300

    style.css ready, javascript done as well, will do some changes later on

---
+++ script.js
@@ -0,0 +1,29 @@
+let navbar = document.querySelector(".navbar");
+
+document.querySelector("#menu-btn").onClick = () => {
+  navbar.classList.toggle("active");
+  searchForm.classList.remove("active");
+  cartItem.classList.remove("active");
+};
+
+let searchForm = document.querySelector(".search-form");
+
+document.querySelector("search-btn").onClick = () => {
+  searchForm.classList.toggle("active");
+  navbar.classList.remove("active");
+  cartItem.classList.remove("active");
+};
+
+let cartItem = document.querySelector(".cart-items-container");
+
+document.querySelector("#cart-btn").onClick = () => {
+  cartItem.classList.toggle("active");
+  navbar.classList.remove("active");
+  searchForm.classList.remove("active");
+};
+
+window.onscroll = () => {
+  navbar.classList.remove("active");
+  searchForm.classList.remove("active");
+  cartItem.classList.remove("active");
+};
